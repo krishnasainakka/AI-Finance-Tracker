378dfa948159112225c41eb12ad67d120223b105
Krishna sai <117358318+krishnasainakka@users.noreply.github.com>
2025-05-29 22:38:42 +0530

    downgraded react from19 to 18 due to conflict issues and changed cors domain

--- backend/index.js
+++ backend/index.js
@@ -17,8 +17,13 @@ const PORT = process.env.PORT || 5000;
 
 connectToMongo();
 
+// app.use(cors({
+//   origin: 'http://localhost:5173',
+//   credentials: true,
+// }));
+
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: '*',
   credentials: true,
 }));
 
