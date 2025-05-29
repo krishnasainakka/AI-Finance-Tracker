2d95a90eacd9b107ff95049bed962166c9e3ebad
Krishna sai <117358318+krishnasainakka@users.noreply.github.com>
2025-05-29 21:45:24 +0530

    changed port to dynamically take

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ import cors from 'cors';
 
 
 const app = express();
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 
 connectToMongo();
 
@@ -37,6 +37,6 @@ app.get('/',(req,res)=>{
     res.send("Hello World")
 })
 
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
